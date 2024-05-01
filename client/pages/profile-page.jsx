5ebe0c813358b89ca15f772880bbfde394fe650f
basswoman <54603761+basswoman@users.noreply.github.com>
2024-05-01 03:17:30 +0300

    saved jobs wip

--- client/pages/profile-page.jsx
+++ client/pages/profile-page.jsx
@@ -1,8 +1,7 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import logo from '../assets/wobbe_mascot2.png';
 import profilePic from '../assets/wobbe_mascot_profile.png';
-
 
 function EditProfile(props) {
   const navigate = useNavigate();
@@ -12,30 +11,30 @@ function EditProfile(props) {
 
   const updateProfile = async () => {
     try {
-      const response = await fetch("/editprofile", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
+      const response = await fetch('/editprofile', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
           email: props.currentEmail,
           newEmail,
           newPassword,
         }),
       });
-      if(response.status === 200) {
-        alert('Profile Updated')
-        if(newEmail !== '') {
-         props.setCurrentEmail(newEmail);
+      if (response.status === 200) {
+        alert('Profile Updated');
+        if (newEmail !== '') {
+          props.setCurrentEmail(newEmail);
         }
         navigate('/home');
       } else {
-        alert('Error updating profile')
+        alert('Error updating profile');
       }
     } catch (error) {
-      console.log("Error updating from edit profile:", error);
+      console.log('Error updating from edit profile:', error);
     }
-  }
+  };
 
-  const handleUpdateProfile = (e) => {
+  const handleUpdateProfile = e => {
     e.preventDefault();
     newEmail = document.getElementById('email').value;
     newPassword = document.getElementById('password').value;
@@ -46,12 +45,24 @@ function EditProfile(props) {
   };
 
   function handlePasswordVisibility() {
-    if(document.getElementById('password').getAttribute('type') === 'password') {
+    if (
+      document.getElementById('password').getAttribute('type') === 'password'
+    ) {
       document.getElementById('password').setAttribute('type', 'text');
-      document.getElementById('passwordImage').setAttribute('src', 'https://media.geeksforgeeks.org/wp-content/uploads/20210917150049/eyeslash.png')
+      document
+        .getElementById('passwordImage')
+        .setAttribute(
+          'src',
+          'https://media.geeksforgeeks.org/wp-content/uploads/20210917150049/eyeslash.png'
+        );
     } else {
       document.getElementById('password').setAttribute('type', 'password');
-      document.getElementById('passwordImage').setAttribute('src', 'https://media.geeksforgeeks.org/wp-content/uploads/20210917145551/eye.png')
+      document
+        .getElementById('passwordImage')
+        .setAttribute(
+          'src',
+          'https://media.geeksforgeeks.org/wp-content/uploads/20210917145551/eye.png'
+        );
     }
   }
 
@@ -84,7 +95,7 @@ function EditProfile(props) {
   //   console.log('newEmail: ', newEmail);
   //   updateEmail();
   // };
-  
+
   // const updatePassword = async () => {
   //   try {
   //     const response = await fetch("/editpassword", {
@@ -105,56 +116,98 @@ function EditProfile(props) {
   //     console.log("Error updating from edit profile:", error);
   //   }
   // }
-  
-    // const handleUpdatePassword = (e) => {
-    //   e.preventDefault();
-    //   newPassword = document.getElementById('password').value;
-    //   console.log('current email: ', props.currentEmail);
-    //   console.log('newPassword: ', newPassword);
-    //   updatePassword();
-    // };
-  
+
+  // const handleUpdatePassword = (e) => {
+  //   e.preventDefault();
+  //   newPassword = document.getElementById('password').value;
+  //   console.log('current email: ', props.currentEmail);
+  //   console.log('newPassword: ', newPassword);
+  //   updatePassword();
+  // };
+
   return (
-    <div className="search-page min-h-screen" style={{ backgroundImage: `url('../assets/AdobeStock_689555388_deepsea.jpeg')`, backgroundRepeat: 'no-repeat', backgroundAttachment: 'fixed' ,height: '100vh'}}>
-      <div className='pl-[5%] pt-[5%]' style={{ zIndex: 30, fontFamily: 'pacifico', color: 'white', fontSize: '4rem' }}>
+    <div
+      className="search-page min-h-screen"
+      style={{
+        backgroundImage: `url('../assets/AdobeStock_689555388_deepsea.jpeg')`,
+        backgroundRepeat: 'no-repeat',
+        backgroundAttachment: 'fixed',
+        height: '100vh',
+      }}
+    >
+      <div
+        className="pl-[5%] pt-[5%]"
+        style={{
+          zIndex: 30,
+          fontFamily: 'pacifico',
+          color: 'white',
+          fontSize: '4rem',
+        }}
+      >
         WobbeJobs
-      </div>  
-      <div className='flex justify-center ml-[-420px] mt-[-110px]'>
+      </div>
+      <div className="flex justify-center ml-[-420px] mt-[-110px]">
         <img
           src={logo}
-          alt= 'tasselled wobbegong shark'
-          className='h-[125px] w-[125px]'
+          alt="tasselled wobbegong shark"
+          className="h-[125px] w-[125px]"
         />
-      </div>   
-        <div className='flex justify-center mb-2'>
-          {/* <div class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+      </div>
+      <div className="flex justify-center mb-2">
+        {/* <div class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
               <svg class="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
           </div> */}
-          <img className='h-[80px] ml-[-37.5px]' src={profilePic}></img>
-        </div>
-      <div className='flex justify-center'>
-        <h1 className='text-3xl font-semibold mb-4 text-white text-center'>Hello, <br/> {props.currentEmail}</h1>
+        <img className="h-[80px] ml-[-37.5px]" src={profilePic}></img>
+      </div>
+      <div className="flex justify-center">
+        <h1 className="text-3xl font-semibold mb-4 text-white text-center">
+          Hello, <br /> {props.showName}
+        </h1>
       </div>
 
-      <div className='flex justify-center'>
-        <div className='max-w-md w-full z-10 bg-white rounded-xl shadow-2xl p-6 bg-blue-200'>
+      <div className="flex justify-center">
+        <div className="max-w-md w-full z-10 bg-white rounded-xl shadow-2xl p-6 bg-blue-200">
           <div className="flex justify-center mb-4">
-            <label className="font-bold text-lg">Enter your new profile information</label>
+            <label className="font-bold text-lg">
+              Enter your new profile information
+            </label>
           </div>
           <label>Email </label>
           <label className="text-gray-500 italic">(optional)</label>
           <label>:</label>
-          <input type="email" id="email" name="username" placeholder="Enter new Email:" className='pl-2 mt-1 block w-full rounded-md shadow-sm'/>
+          <input
+            type="email"
+            id="email"
+            name="username"
+            placeholder="Enter new Email:"
+            className="pl-2 mt-1 block w-full rounded-md shadow-sm"
+          />
           {/* <button onClick={handleUpdateEmail}>Update Email</button> */}
           <br />
           <label className="password-login">Password </label>
           <label className="text-gray-500 italic">(optional)</label>
           <label>:</label>
-          <input type="password" id="password" name="password" placeholder="Enter new Password:" className='pl-2 mt-1 block w-full rounded-md shadow-sm'/>
-          <img id='passwordImage' src='https://media.geeksforgeeks.org/wp-content/uploads/20210917145551/eye.png' className='h-5 w-5 inline-block ml-[93%] mt-[-13%]' onClick={handlePasswordVisibility}></img>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Enter new Password:"
+            className="pl-2 mt-1 block w-full rounded-md shadow-sm"
+          />
+          <img
+            id="passwordImage"
+            src="https://media.geeksforgeeks.org/wp-content/uploads/20210917145551/eye.png"
+            className="h-5 w-5 inline-block ml-[93%] mt-[-13%]"
+            onClick={handlePasswordVisibility}
+          ></img>
           {/* <button onClick={handleUpdatePassword}>Update Password</button> */}
-          <div className='flex justify-center mt-4'>
-            <button onClick={handleUpdateProfile} className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-50 hover:text-blue-500 w-auto h-9" >Update Profile</button>
+          <div className="flex justify-center mt-4">
+            <button
+              onClick={handleUpdateProfile}
+              className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-50 hover:text-blue-500 w-auto h-9"
+            >
+              Update Profile
+            </button>
           </div>
         </div>
       </div>
